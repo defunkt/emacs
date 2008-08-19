00dee28cd244603b4afccfea779027240e304ef0
Chris Wanstrath <chris@ozmm.org>
2008-08-19 04:15:03 -0700

    add readme.markdown, fix up .emacs

---
+++ README.markdown
@@ -0,0 +1,14 @@
+= defunkt's .emacs
+
+On 2008-08-17 I decided to switch from Vim to Emacs.
+
+My primary motivation was a desire for extensibility, rather than
+scriptability.
+
+Also, elisp > vimscript.
+
+This repository is my configuration.  
+
+It will grow as I grow. 
+
+(chris@ozmm.org) ; chris wanstrath
--- emacs.el
+++ emacs.el
@@ -1,5 +1,15 @@
+; global
+(add-to-list 'load-path "~/.emacs.d/")
+
 ; rinari
-(add-to-list 'load-path "~/.emacs.d/rinari")
+(add-to-list 'load-path "~/.emacs.d/rinari/")
 (require 'rinari)
 (global-set-key (kbd "C-x C-M-f") 'find-file-in-project) ;; optional
 (setq rinari-tags-file-name "TAGS")
+
+; markdown
+(add-to-list 'load-path "~/.emacs.d/markdown-mode")
+(autoload 'markdown-mode "markdown-mode.el"
+   "Major mode for editing Markdown files" t)
+(setq auto-mode-alist
+   (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
