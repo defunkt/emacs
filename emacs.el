1846d1a2fd2d6580a9f2a0be5208ca042b3a2b5b
Chris Wanstrath <chris@ozmm.org>
2008-08-28 10:20:45 -0700

    Add mode-compile and ruby-hacks (based on rails-ruby -- which apparently has nothing to do with Rails)

--- emacs.el
+++ emacs.el
@@ -83,9 +83,32 @@
 (add-to-list 'load-path "~/.emacs.d/rhtml")
 (require 'rhtml-mode)
 (add-hook 'rhtml-mode-hook 'rinari-launch)
+(setq auto-mode-alist (cons '("\\.html\\.erb" . rhtml-mode) auto-mode-alist))
 
 ; ruby
+(require 'ruby-hacks)
 (setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (add-hook 'local-write-file-hooks
+                      '(lambda()
+                         (save-excursion
+                           (untabify (point-min) (point-max))
+                           (delete-trailing-whitespace))))
+            (set (make-local-variable 'indent-tabs-mode) 'nil)
+            (set (make-local-variable 'tab-width) 2)
+            (define-key ruby-mode-map "\C-m" 'newline-and-indent)
+            (require 'ruby-electric)
+            (ruby-electric-mode t)))
+
+; mode-compile
+(autoload 'mode-compile "mode-compile"
+   "Command to compile current buffer file based on the major mode" t)
+(global-set-key "\C-cc" 'mode-compile)
+
+(autoload 'mode-compile-kill "mode-compile"
+ "Command to kill a compilation launched by `mode-compile'" t)
+(global-set-key "\C-ck" 'mode-compile-kill)
 
 ; treetop
 (require 'treetop)
