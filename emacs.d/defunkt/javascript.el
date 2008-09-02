e0962aefe34e93733a758084a37947e8c1766509
Chris Wanstrath <chris@ozmm.org>
2008-09-01 22:13:29 -0700

    execute js2 buffer with johnson

--- emacs.d/defunkt/javascript.el
+++ emacs.d/defunkt/javascript.el
@@ -4,3 +4,10 @@
 
 ; js-shell
 (autoload 'javascript-shell "javascript-mode" nil t)
+
+(defun js2-execute-buffer () 
+  (interactive)
+  (shell-command (concat "johnson " (buffer-file-name))))
+
+(add-hook 'js2-mode-hook '(lambda () 
+                            (define-key js2-mode-map [A-r] 'js2-execute-buffer)))
