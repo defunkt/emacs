2c5b9b0c372ed43bffc2f3dcf158592d5415a57b
Chris Wanstrath <chris@ozmm.org>
2008-10-02 09:12:03 -0700

    add up / down to shell mode

--- emacs.d/defunkt/shell.el
+++ emacs.d/defunkt/shell.el
@@ -3,5 +3,7 @@
 
 (eval-after-load 'shell
   '(progn
+     (define-key shell-mode-map [up] 'comint-previous-input)
+     (define-key shell-mode-map [down] 'comint-next-input)
      (define-key shell-mode-map "\C-p" 'comint-previous-input)
      (define-key shell-mode-map "\C-n" 'comint-next-input)))
