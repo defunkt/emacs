072308fb8e21f2e36bcb58264f7a2a5d8200401e
Chris Wanstrath <chris@ozmm.org>
2008-11-21 14:59:55 -0800

    textmatey keybindings

--- emacs.d/defunkt/helpers.el
+++ emacs.d/defunkt/helpers.el
@@ -8,6 +8,8 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-g" 'gist-buffer)
+(global-set-key (kbd "A-M-]") 'align)
+(global-set-key (kbd "A-]") 'indent-region)
 
 ; no printing!
 (define-key osx-key-mode-map (kbd "A-p") 
