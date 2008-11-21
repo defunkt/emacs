331d11cbff18bcb798e0740214fe6590f5536bdc
Chris Wanstrath <chris@ozmm.org>
2008-11-20 19:18:05 -0800

    wow, love ido-goto-symbol

--- emacs.d/defunkt/helpers.el
+++ emacs.d/defunkt/helpers.el
@@ -42,6 +42,37 @@
   (backward-char))
 (ad-activate 'zap-to-char)
 
+(defun ido-goto-symbol ()
+  "Will update the imenu index and then use ido to select a symbol to navigate to"
+  (interactive)
+  (imenu--make-index-alist)
+  (let ((name-and-pos '())
+        (symbol-names '()))
+    (flet ((addsymbols (symbol-list)
+                       (when (listp symbol-list)
+                         (dolist (symbol symbol-list)
+                           (let ((name nil) (position nil))
+                             (cond
+                              ((and (listp symbol) (imenu--subalist-p symbol))
+                               (addsymbols symbol))
+                              
+                              ((listp symbol)
+                               (setq name (car symbol))
+                               (setq position (cdr symbol)))
+                              
+                              ((stringp symbol)
+                               (setq name symbol)
+                               (setq position (get-text-property 1 'org-imenu-marker symbol))))
+                             
+                             (unless (or (null position) (null name))
+                               (add-to-list 'symbol-names name)
+                               (add-to-list 'name-and-pos (cons name position))))))))
+      (addsymbols imenu--index-alist))
+    (let* ((selected-symbol (ido-completing-read "Symbol? " symbol-names))
+           (position (cdr (assoc selected-symbol name-and-pos))))
+      (goto-char position))))
+(global-set-key "\C-x\C-i" 'ido-goto-symbol)
+
 (defun defunkt-find-config ()
   (interactive)
   (let ((config-file
