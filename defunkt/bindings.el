37ec95c0ef735de5c35c9972bc023e05d89cf4d6
Chris Wanstrath <chris@ozmm.org>
2008-11-21 16:36:16 -0800

    textmate style command+t and command+shift+t bindings

--- defunkt/bindings.el
+++ defunkt/bindings.el
@@ -1,22 +1,27 @@
 (global-set-key "\C-x\C-b" 'buffer-menu)
-(global-set-key [C-tab] 'other-window) ; vimy window switching
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-g" 'gist-buffer)
-(global-set-key (kbd "A-M-]") 'align)
-(global-set-key (kbd "A-]") 'indent-region)
 (global-set-key "\C-xg" 'magit-status)
 (global-set-key "\M-i" 'insert-soft-tab)
-(global-set-key "\C-x\C-i" 'ido-goto-symbol)
 (global-set-key "\C-xp" 'defunkt-find-config)
-(global-set-key [remap kill-word] 'defunkt-kill-word)
-(global-set-key [remap backward-kill-word] 'defunkt-backward-kill-word)
-(global-set-key [remap aquamacs-backward-kill-word] 'defunkt-backward-kill-word)
-(global-set-key [A-return] 'insert-blank-line-after-current)
 (global-set-key [C-return] 'defunkt-duplicate-line)
+
+; vim emulation
+(global-set-key [C-tab] 'other-window) 
 (global-set-key [M-up] 'defunkt-inc-num-at-point)
 (global-set-key [M-down] 'defunkt-dec-num-at-point)
 (global-set-key (kbd "C-*") 'isearch-forward-at-point)
+(global-set-key [remap kill-word] 'defunkt-kill-word)
+(global-set-key [remap backward-kill-word] 'defunkt-backward-kill-word)
+(global-set-key [remap aquamacs-backward-kill-word] 'defunkt-backward-kill-word)
+
+; textmate emulation
+(global-set-key [A-return] 'insert-blank-line-after-current)
+(global-set-key (kbd "A-M-]") 'align)
+(global-set-key (kbd "A-]") 'indent-region)
+(define-key osx-key-mode-map (kbd "A-t") 'fuzzy-find-in-project)
+(define-key osx-key-mode-map (kbd "A-T") 'ido-goto-symbol)
 
 ; no printing!
 (define-key osx-key-mode-map (kbd "A-p") 
