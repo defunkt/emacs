4d58c2a73ac195501b498f0a7a8888bc35bffdc9
Chris Wanstrath <chris@ozmm.org>
2008-11-22 17:43:43 -0800

    capfile is ruby

--- defunkt/ruby.el
+++ defunkt/ruby.el
@@ -11,6 +11,7 @@
 ; ruby
 (vendor 'ruby-hacks)
 (setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Capfile" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.rake" . ruby-mode) auto-mode-alist))
 (add-hook 'ruby-mode-hook
           (lambda ()
