fdee6015fa5c35ad4a1117d8cf711aefee536484
Chris Wanstrath <chris@ozmm.org>
2009-01-24 19:24:34 -0800

    upgraded to aquamacs 1.6 - new way of setting fonts

--- defunkt/custom.el
+++ defunkt/custom.el
@@ -5,19 +5,24 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(aquamacs-additional-fontsets nil t)
- '(aquamacs-customization-version-id 151 t)
+ '(aquamacs-customization-version-id 163 t)
  '(cua-mode nil nil (cua-base))
+ '(default-frame-alist (quote ((tool-bar-lines . 0) (foreground-color . "white") (background-color . "black") (menu-bar-lines . 1) (font . "-apple-inconsolata-medium-r-normal--20-160-72-72-m-160-iso10646-1"))))
  '(erc-modules (quote (autojoin button completion fill irccontrols match menu netsplit noncommands readonly ring scrolltobottom stamp track)))
  '(javascript-shell-command "johnson")
  '(js2-auto-indent-flag nil)
  '(js2-bounce-indent-flag t)
  '(js2-enter-indents-newline t)
  '(js2-strict-missing-semi-warning nil)
- '(one-buffer-one-frame-mode nil nil (aquamacs-frame-setup))
+ '(tabbar-mode nil nil (tabbar))
  '(transient-mark-mode t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :family "apple-inconsolata"))))
+ '(emacs-lisp-mode-default ((t (:inherit autoface-default :slant normal :weight normal :height 160 :family "inconsolata"))) t)
+ '(minibuffer-prompt ((((background dark)) (:foreground "cyan" :height 160))))
+ '(mode-line ((t (:background "grey75" :foreground "black" :height 140))))
+ '(text-mode-default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 160 :width normal :family "inconsolata"))) t))
