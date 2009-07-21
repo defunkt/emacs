bde0efee9421bd46fc8dc2d226aa3ab95df4939c
Chris Wanstrath <chris@ozmm.org>
2009-07-21 10:52:32 -0700

    no warnings when compiling ruby

--- defunkt/ruby.el
+++ defunkt/ruby.el
@@ -13,6 +13,10 @@
 (setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("Capfile" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.rake" . ruby-mode) auto-mode-alist))
+
+;; no warnings when compiling
+(setq ruby-dbg-flags "")
+
 (add-hook 'ruby-mode-hook
           (lambda ()
             (add-hook 'local-write-file-hooks
