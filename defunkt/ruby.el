e63e2045c5d42795d4984c2a93ea2869f9b923f4
Chris Wanstrath <chris@ozmm.org>
2009-08-28 21:58:11 -0700

    erb and god are ruby too

--- defunkt/ruby.el
+++ defunkt/ruby.el
@@ -7,12 +7,14 @@
 
 ; rhtml
 (setq auto-mode-alist (cons '("\\.html\\.erb" . nxml-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.erb" . nxml-mode) auto-mode-alist))
 
 ; ruby
 (vendor 'ruby-hacks)
 (setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("Capfile" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.rake" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.god" . ruby-mode) auto-mode-alist))
 
 ;; no warnings when compiling
 (setq ruby-dbg-flags "")
