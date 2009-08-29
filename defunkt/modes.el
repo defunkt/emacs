c86caa4f9195d4e73541006d820fa4a45ee06af6
Chris Wanstrath <chris@ozmm.org>
2009-08-29 01:17:09 -0700

    .md is markdown, not obj-c

--- defunkt/modes.el
+++ defunkt/modes.el
@@ -26,6 +26,7 @@
 (autoload 'markdown-mode "markdown-mode.el"
    "Major mode for editing Markdown files" t)
 (setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
 (add-hook 'markdown-mode-hook 
           '(lambda () 
              (define-key markdown-mode-map (kbd "<tab>") 'defunkt-indent)))
