00ee1a7fd6dd68f781347fbbbda4d05ca13300ff
Chris Wanstrath <chris@ozmm.org>
2009-09-10 22:40:27 -0700

    install handles the submodules now

--- restore.rb
+++ restore.rb
@@ -6,6 +6,4 @@ File.open('local.el', 'w') do |f|
   f.puts '(load "defunkt")'
 end
 
-`git submodule update --init`
-`cd vendor/rinari && git submodule update --init`
 puts "Don't forget to M-x byte-compile-file js2"
