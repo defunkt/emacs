219249b76f9e25e8821b763f2e8ecf5b3d95005d
Chris Wanstrath <chris@ozmm.org>
2009-10-10 18:42:39 -0700

    Improved Ruby commenting

--- defunkt/ruby.el
+++ defunkt/ruby.el
@@ -40,13 +40,28 @@
 
 ; where'd this go?
 (defun ruby-reindent-then-newline-and-indent ()
+  "Reindents the current line then creates an indented newline."
   (interactive "*")
   (newline)
   (save-excursion
     (end-of-line 0)
     (indent-according-to-mode)
     (delete-region (point) (progn (skip-chars-backward " \t") (point))))
+  (when (ruby-previous-line-is-comment)
+      (insert "# "))
   (indent-according-to-mode))
+
+(defun ruby-previous-line-is-comment ()
+  "Returns `t' if the previous line is a Ruby comment."
+  (save-excursion
+    (forward-line -1)
+    (ruby-line-is-comment)))
+
+(defun ruby-line-is-comment ()
+  "Returns `t' if the current line is a Ruby comment."
+  (save-excursion
+    (beginning-of-line)
+    (string= "#" (char-to-string (char-after)))))
 
 ; treetop
 (vendor 'treetop)
