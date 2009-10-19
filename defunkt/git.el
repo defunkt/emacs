19345b5c70e057836dbee28e9d1f7ffaf0ed49fd
Chris Wanstrath <chris@ozmm.org>
2009-10-19 13:29:25 -0700

    disable my git hacks for now

--- defunkt/git.el
+++ defunkt/git.el
@@ -14,24 +14,24 @@
 (defun defunkt-set-mode-line (mode-line)
   ;; this has to be built in...
   (setq mode-line-format
-        (append 
-         (butlast mode-line-format (- (length mode-line-format) 8)) 
+        (append
+         (butlast mode-line-format (- (length mode-line-format) 8))
          (cons mode-line (nthcdr 8 mode-line-format)))))
 
 (defun defunkt-git-current-branch (root)
-  (let ((result) (branches 
-         (split-string 
-          (shell-command-to-string 
-           (concat 
-            "git --git-dir=" 
-            root 
-            ".git branch --no-color")) 
+  (let ((result) (branches
+         (split-string
+          (shell-command-to-string
+           (concat
+            "git --git-dir="
+            root
+            ".git branch --no-color"))
           "\n" t)))
-    (while (and branches (null result)) 
+    (while (and branches (null result))
       (if (string-match "^\* \\(.+\\)" (car branches))
           (setq result (match-string 1 (car branches)))
         (setq branches (cdr branches))))
     result))
 
-(add-hook 'find-file-hook 'defunkt-git-modeline)
-(add-hook 'dired-after-readin-hook 'defunkt-git-modeline)
+;; (add-hook 'find-file-hook 'defunkt-git-modeline)
+;; (add-hook 'dired-after-readin-hook 'defunkt-git-modeline)
