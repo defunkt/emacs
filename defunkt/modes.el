17eec4f0b9b0e66816b6a2fd95fa7f5fa077b97d
Chris Wanstrath <chris@ozmm.org>
2009-12-16 18:28:41 -0800

    ron is markdown

--- defunkt/modes.el
+++ defunkt/modes.el
@@ -31,6 +31,7 @@
    "Major mode for editing Markdown files" t)
 (setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.ron" . markdown-mode) auto-mode-alist))
 (add-hook 'markdown-mode-hook '(lambda ()
                                  (define-key markdown-mode-map (kbd "A-r") 'markdown-preview)
                                  (define-key markdown-mode-map (kbd "<tab>") 'defunkt-indent)))
