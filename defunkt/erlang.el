ebfae2a6fddb359f2c16bec4060abce00403533a
Chris Wanstrath <chris@ozmm.org>
2010-03-02 16:16:34 -0800

    make it easier to change the emacs version for tool loading

--- defunkt/erlang.el
+++ defunkt/erlang.el
@@ -1,7 +1,9 @@
-(when (file-exists-p "/usr/local/lib/erlang/lib/tools-2.6.1/emacs") 
-  (setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.6.1/emacs"
-                         load-path))
-  (setq erlang-root-dir "/usr/local/lib/erlang")
-  (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
-  (require 'erlang-start)
-  (defvar inferior-erlang-prompt-timeout t))
+(let* ((emacs-version "2.6.4")
+       (tools-path
+        (concat "/usr/local/lib/erlang/lib/tools-" emacs-version "/emacs")))
+  (when (file-exists-p tools-path)
+    (setq load-path (cons tools-path load-path))
+    (setq erlang-root-dir "/usr/local/lib/erlang")
+    (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
+    (require 'erlang-start)
+    (defvar inferior-erlang-prompt-timeout t)))
