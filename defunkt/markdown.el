bcb88e56c86880cb6e75866afe863aed0583ee10
Chris Wanstrath <chris@ozmm.org>
2010-03-09 10:12:32 -0800

    split out markdown hacks

---
+++ defunkt/markdown.el
@@ -0,0 +1,27 @@
+(add-to-list 'load-path "~/.emacs.d/vendor/markdown-mode")
+(autoload 'markdown-mode "markdown-mode.el"
+   "Major mode for editing Markdown files" t)
+
+(setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.ron" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.ronn" . markdown-mode) auto-mode-alist))
+
+(defun defunkt-markdown-insert-link ()
+  "Inserts an awesome link: [Word][ref] with [ref]: url at the
+bottom. Prompts you, for sure."
+  (interactive)
+  (insert "[")
+  (save-excursion
+    (insert "]")
+    (insert "[]")
+
+)
+)
+
+(add-hook
+ 'markdown-mode-hook
+ '(lambda ()
+    (define-key markdown-mode-map (kbd "C-c i") 'defunkt-markdown-insert-link)
+    (define-key markdown-mode-map (kbd "A-r") 'markdown-preview)
+    (define-key markdown-mode-map (kbd "<tab>") 'defunkt-indent)))
--- defunkt/modes.el
+++ defunkt/modes.el
@@ -9,6 +9,7 @@
 (load "defunkt/erc")
 (load "defunkt/artist")
 (load "defunkt/coffee")
+(load "defunkt/markdown")
 
 ;; all modes
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
@@ -28,18 +29,6 @@
   '(progn
      (set-face-foreground 'magit-diff-add "green3")
      (set-face-foreground 'magit-diff-del "red3")))
-
-; markdown
-(add-to-list 'load-path "~/.emacs.d/vendor/markdown-mode")
-(autoload 'markdown-mode "markdown-mode.el"
-   "Major mode for editing Markdown files" t)
-(setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.ron" . markdown-mode) auto-mode-alist))
-(add-hook 'markdown-mode-hook '(lambda ()
-                                 (define-key markdown-mode-map (kbd "A-r") 'markdown-preview)
-                                 (define-key markdown-mode-map (kbd "<tab>") 'defunkt-indent)))
-
 
 ; paredit
 (autoload 'paredit-mode "paredit"
