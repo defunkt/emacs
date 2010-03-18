4e95d7d7b3bf6d597fe2156509d291950fe9df9a
Chris Wanstrath <chris@ozmm.org>
2010-03-17 21:40:25 -0700

    Compile .coffee to .js on save

--- defunkt/coffee.el
+++ defunkt/coffee.el
@@ -1,5 +1,9 @@
 (defun defunkt-coffee-mode-hook ()
   "defunkt's hacks and experiments for `coffee-mode.el'."
+
+  ;; Compile .js on every save.
+  (add-hook 'before-save-hook 'coffee-compile-file)
+
   ;; I like debug mode, sometimes.
   (setq coffee-debug-mode nil))
 
