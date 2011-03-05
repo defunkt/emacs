c308a3052124cc0d9a81800f230d3036cd90a2a0
Chris Wanstrath <chris@ozmm.org>
2011-03-05 00:50:37 -0800

    un-break css, mostly

--- defunkt/css.el
+++ defunkt/css.el
@@ -1,15 +1,22 @@
 ;; css
-(setq css-mode-indent-depth 2)
+
 (add-hook 'css-mode-hook '(lambda ()
-                            (define-key css-mode-map "\C-s" 'css-new-section)
+                            (setq css-mode-indent-depth 2)
+                            (make-local-variable 'indent-line-function)
+                            (setq indent-line-function 'css-indent)
+                            (define-key css-mode-map "\C-ct" 'css-insert-section)
                             (define-key css-mode-map [tab] 'defunkt-indent)))
+
+(defun css-indent ()
+  (interactive)
+  (insert "  "))
 
 (defun css-insert-section (section)
   "Inserts a kyle-style css section."
   (interactive "sSection: ")
-  (insert "/*------------------------------------------------------------------------------\n")
+  (insert "/*--------------------------------------------------------------------\n")
   (insert (concat "  @group " section "\n"))
-  (insert "------------------------------------------------------------------------------*/\n")
+  (insert "--------------------------------------------------------------------*/\n")
   (insert "\n")
   (insert "\n")
   (insert "\n")
