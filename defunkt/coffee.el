4854f9345f2b838c3a1bef6772c4c3b1cb2865a0
Chris Wanstrath <chris@ozmm.org>
2011-03-06 15:56:46 -0800

    make textmate mode play nicer with coffee mode

--- defunkt/coffee.el
+++ defunkt/coffee.el
@@ -2,12 +2,30 @@
   "defunkt's hacks and experiments for `coffee-mode.el'."
 
   ;; Compile .js on every save.
-  (add-hook 'before-save-hook
-            '(lambda ()
-               (when (not (string= (buffer-name) "Cakefile"))
-                 (shell-command "cake build"))))
+;;   (add-hook 'before-save-hook
+;;             '(lambda ()
+;;                (when (not (string= (buffer-name) "Cakefile"))
+;;                  (shell-command "cake build"))))
+
+
+
+  ;; aww yeah
+  (define-key coffee-mode-map "\C-L" 'coffee-insert-console)
 
   ;; I like debug mode, sometimes.
   (setq coffee-debug-mode nil))
 
 (add-hook 'coffee-mode-hook (lambda () (defunkt-coffee-mode-hook)))
+
+(defadvice textmate-next-line (around coffee-tm-next-line)
+  (interactive)
+  (if (string= major-mode "coffee-mode")
+      (progn
+        (end-of-line)
+        (coffee-newline-and-indent))
+    ad-do-it))
+(ad-activate 'textmate-next-line)
+
+(defun coffee-insert-console ()
+  (interactive)
+  (insert "console.log "))
