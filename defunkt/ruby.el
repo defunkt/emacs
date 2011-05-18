80059207ae24f590a24f9b80823a6186ca8f1eb5
Chris Wanstrath <chris@ozmm.org>
2011-05-17 17:14:02 -0700

    gemfile

--- defunkt/ruby.el
+++ defunkt/ruby.el
@@ -13,6 +13,7 @@
 (vendor 'ruby-hacks)
 (setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("Capfile" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Gemfile" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.rake" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.god" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.ru" . ruby-mode) auto-mode-alist))
