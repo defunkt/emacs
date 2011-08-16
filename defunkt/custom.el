d78475502b5e2dc8fb6b30b80c45df7a16f8f73a
Chris Wanstrath <chris@ozmm.org>
2011-08-16 15:01:13 -0700

    bigger font

--- defunkt/custom.el
+++ defunkt/custom.el
@@ -7,7 +7,7 @@
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 190 t)
  '(cua-mode nil nil (cua-base))
- '(default-frame-alist (quote ((tool-bar-lines . 0) (foreground-color . "white") (background-color . "black") (menu-bar-lines . 1) (font . "-apple-inconsolata-medium-r-normal--20-150-72-72-m-150-iso10646-1"))))
+ '(default-frame-alist (quote ((tool-bar-lines . 0) (foreground-color . "white") (background-color . "black") (menu-bar-lines . 1) (font . "-apple-inconsolata-medium-r-normal--20-180-72-72-m-180-iso10646-1"))))
  '(erc-modules (quote (autojoin button completion fill irccontrols match menu netsplit noncommands readonly ring scrolltobottom stamp track)))
  '(javascript-shell-command "johnson")
  '(js2-auto-indent-flag nil)
@@ -29,10 +29,10 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :family "apple-inconsolata"))))
- '(autoface-default ((t (:inherit default :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :family "apple-inconsolata"))))
+ '(default ((t (:stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :family "apple-inconsolata"))))
+ '(autoface-default ((t (:inherit default :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :family "apple-inconsolata"))))
  '(emacs-lisp-mode-default ((t (:inherit autoface-default))) t)
  '(js2-mode-default ((t (:inherit autoface-default))) t)
- '(minibuffer-prompt ((((background dark)) (:foreground "cyan" :height 150))))
+ '(minibuffer-prompt ((((background dark)) (:foreground "cyan" :height 180))))
  '(mode-line ((t (:inherit aquamacs-variable-width :background "grey75" :foreground "black" :width normal))))
  '(text-mode-default ((t (:inherit autoface-default))) t))
