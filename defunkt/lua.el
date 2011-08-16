b20f9a012bc6099a904949744f2e68e3dffe29bb
Chris Wanstrath <chris@ozmm.org>
2011-08-16 13:58:25 -0700

    lua

--- defunkt/lua.el
+++ defunkt/lua.el
@@ -1,6 +1,20 @@
 ;; use an indentation width of two spaces
 (setq lua-indent-level 2)
 
+(defun lua-insert-puts ()
+  (interactive)
+  (insert "puts()")
+  (backward-char))
+
+(defun lua-rake-run ()
+  (interactive)
+  (shell-command "rake run"))
+
+(add-hook 'lua-mode-hook
+          '(lambda ()
+             (define-key lua-mode-map (kbd "A-r") 'lua-rake-run)
+             (define-key lua-mode-map "\C-L" 'lua-insert-puts)))
+
 ;; Add dangling '(', remove '='
 ;; (setq lua-cont-eol-regexp
 ;;       (eval-when-compile
